f2aa8a9fe2b6ab5f9c24e8092aa124c3efb61ad0
Rustam <rustamdvl@yandex.ru>
2022-10-13 21:34:05 +0500

    сделано 4 задание

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,61 @@ import java.util.Scanner;
 
 final class Main {
     public static void main(String[] args){
-        doStackOperation(new File("input.txt"));
+        //doStackOperation(new File("input.txt"));
+
+        calculatePostfix(new File("4task.txt"));
     }
+
+
+    static void calculatePostfix(File file){
+        try(FileReader reader = new FileReader(file.getAbsolutePath()))
+        {
+            Scanner scanner = new Scanner(reader);
+            var commands = scanner.nextLine().split(" ");
+            Stack<Double> stack = new Stack<>();
+            for (var i:commands)
+            {
+                if (i.charAt(0) >= '0' && i.charAt(0) <= '9')
+                {
+                    stack.push(Double.parseDouble(i));
+                }
+                else
+                {
+                    switch (i) {
+                        case "+" -> stack.push(stack.pop() + stack.pop());
+                        case "-" -> {
+                            var a = stack.pop();
+                            var b = stack.pop();
+                            stack.push(b - a);
+                        }
+                        case "*" -> stack.push(stack.pop() * stack.pop());
+                        case "/", ":" -> {
+                            var a = stack.pop();
+                            var b = stack.pop();
+                            stack.push(b / a);
+                        }
+                        case "^" -> {
+                            var a = stack.pop();
+                            var b = stack.pop();
+                            stack.push(Math.pow(b, a));
+                        }
+                        case "ln" -> {
+                            var a = stack.pop();
+                            stack.push(Math.log(a));
+                        }
+                        case "cos" -> stack.push(Math.cos(stack.pop()));
+                        case "sin" -> stack.push(Math.sin(stack.pop()));
+                        case "sqrt" -> stack.push(Math.sqrt(stack.pop()));
+                    }
+                }
+            }
+            System.out.println(stack.pop());
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
     static void doStackOperation(File file){
 
         try(FileReader reader = new FileReader(file.getAbsolutePath()))
@@ -18,8 +71,7 @@ final class Main {
             for (var i:commands) {
                 if(i.charAt(0) == '1')
                 {
-                    var elem = i.split(",");
-                    stack.push(elem[1]);
+                    stack.push(i.split(",")[1]);
                     System.out.println(stack.top());
                     stepCount++;
                 }
